185578707056f45e15418618c03c48c66e451397
Rattapoom4796 <yukari4796@gmail.com>
2024-07-03 11:52:06 +0700

    first commit

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const users = require("./db.json");
 const app = express();
 const port = process.env.PORT || 3000; 
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
 app.get('/api/users', (req, res) => {
 
@@ -13,15 +15,31 @@ app.get('/api/users', (req, res) => {
  app.get('/api/users/:id', (req, res) => {
 
     res.json(users.find(user => user.id === Number(req.params.id)))
+ })
+
+ app.post('/api/users', (req, res) => { 
+    users.push(req.body);
+    let json = req.body;
+    console.log(json);
+    res.send("Username: " +json.username +"inserted.");
+})
+
+app.put('/api/users/:id', (req, res) => {
+    const updateIndex = users.findIndex(user => user.id ===
+ Number(req.params.id)) 
+     res.send(`Update user id: '${users[updateIndex].id}' completed.`)
+ 
+ })
+ app.delete('/users/:id', (req, res) => {
+    const deletedIndex = users.findIndex(user => user.id ===
+ Number(req.params.id)) 
+    res.send(`Delete user '${users[deletedIndex].username}' completed.`)
  
  })
 
 
 
-
 app.get("/Rattapoom", (req, res) => { res.send("Hello! My name is Rattapoom.");});
-
-
 app.listen(port, () => {
     console.log("Starting node.js at http://127.0.0.1:3000/ " + port + "/Rattapoom");
     console.log("Starting node.js at http://127.0.0.1:3000/ " + port + "/api/users");
